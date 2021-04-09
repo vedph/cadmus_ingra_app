c17d941dd9b830b36791ab12449785cc62e64d3a
Myrmex <dfusi@hotmail.com>
2021-04-09 10:57:54 +0200

    Updated packages and added lookup defs

--- src/app/index-lookup-definitions.ts
+++ src/app/index-lookup-definitions.ts
@@ -1,3 +1,16 @@
 import { IndexLookupDefinitions } from '@myrmidon/cadmus-core';
 
-export const INDEX_LOOKUP_DEFINITIONS : IndexLookupDefinitions = {}
+export const INDEX_LOOKUP_DEFINITIONS : IndexLookupDefinitions = {
+  prison: {
+    typeId: 'it.vedph.ingra.prison-info',
+    name: 'id'
+  },
+  prisoner: {
+    typeId: 'it.vedph.ingra.prisoner-info',
+    name: 'id'
+  },
+  graffiti: {
+    typeId: 'it.vedph.ingra.graffiti-info',
+    name: 'id'
+  }
+}
